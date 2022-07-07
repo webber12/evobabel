9b105c008277fb794052e5bed0d6fbc6f6b0bedd
webber12 <web-ber12@yandex.ru>
2022-07-07 12:21:37 +0300

    init

---
+++ src/EvoBabelServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace EvolutionCMS\EvoBabel;
+
+use EvolutionCMS\ServiceProvider;
+
+class EvoBabelServiceProvider extends ServiceProvider
+{
+    protected $namespace = 'evobabel';
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+    }
+
+}
